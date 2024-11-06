639969c37b0f3700368667e024b8e7376e0e3440
ignacioaj131982 <ignacioarevalo@gmail.com>
2024-11-06 20:48:53 -0300

    Primer comit del parcial Front End III

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/card";
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+
+}
+
+export default App
---
+++ src/components/card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+console.log(React);
+
+
+
+const Card = () => {
+  return <div>Hola, soy una tarjeta</div>;
+};
+
+export default Card;
